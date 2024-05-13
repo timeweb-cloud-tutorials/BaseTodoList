32c85b48f2820486c8a09395d3414d73724c32c1
d.bahtenkov <d.bahtenkov@skillaz.ru>
2024-05-13 17:25:33 +0300

    update gitignore

---
+++ ToDoList.App/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.App.Models;
+
+namespace ToDoList.App.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return RedirectToAction("Index", "ToDo");
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+}
