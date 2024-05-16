843aec78d47fddb6d5479dc6843d1c6bfd3654a6
d.bahtenkov <d.bahtenkov@skillaz.ru>
2024-05-16 20:43:45 +0300

    changes

---
+++ BaseToDoList/Data/TodoListDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoList.App.Models;
+
+namespace ToDoList.App.Data;
+
+public sealed class TodoListDbContext : DbContext
+{
+    public TodoListDbContext(DbContextOptions<TodoListDbContext> options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
+
+    public DbSet<TodoItem> ToDoItems { get; set; }
+}
