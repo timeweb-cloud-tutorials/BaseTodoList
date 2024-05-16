127bf851b9f13715588b16ace818c3eb404537b4
d.bahtenkov <d.bahtenkov@skillaz.ru>
2024-05-16 20:43:12 +0300

    renamings

--- ToDoList.App/Models/TodoItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ToDoList.App.Models;
-
-public class TodoItem
-{
-    public int Id { get; set; }
-    public string Title { get; set; }
-    public bool IsCompleted { get; set; }
-    public DateTime CreatedAt { get; set; }
-}
